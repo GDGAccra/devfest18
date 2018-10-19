72ddd3f7c39780c5a74900eddecd55dac2ed623e
Jeffrey Godwyll <jeffgodwyll@gmail.com>
2018-10-19 22:00:50 +0000

    firebase settings for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA-YeJiCSrwtV2GrhL1LoX-2QPVKBRPpNc',
+    authDomain: 'devfestgh-18.firebaseapp.com',
+    databaseURL: 'https://devfestgh-18.firebaseio.com',
+    projectId: 'devfestgh-18',
+    storageBucket: 'devfestgh-18.appspot.com',
+    messagingSenderId: '45482651854'
+  }
 };
